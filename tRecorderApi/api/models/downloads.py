2a058fa69356a4f9a5e32f08d41098ac823404d6
mxaln <murmax82@gmail.com>
2019-05-06 16:47:09 -0400

    added is_social to user creation;

--- tRecorderApi/api/models/downloads.py
+++ tRecorderApi/api/models/downloads.py
@@ -1,5 +1,4 @@
 class Downloads(object):
     def __init__(self, **kwargs):
-        for field in (
-                'name','url'):
+        for field in ('name','url'):
             setattr(self, field, kwargs.get(field, None))
